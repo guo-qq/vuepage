d975549235d0089b3673855d5e9642beee2ef79d
2282143659@qq.com <2282143659@qq.com>
2021-03-03 10:45:40 +0800

    master

--- src/components/Inventory/InventorySels.vue
+++ src/components/Inventory/InventorySels.vue
@@ -1,5 +1,40 @@
 <template>
-  <el-table
+<div>
+  <div id="app">
+    <!--仓库选择-->
+    <div style="float:left">    
+        <span class="demonstration">仓库</span>
+    <el-cascader
+      v-model="value3"
+      :options="options"
+      :props="{ expandTrigger: 'hover' }"
+      @change="handleChange" value="0">请选择</el-cascader>
+ 
+  &nbsp;&nbsp;&nbsp;</div>
+
+  <!--创建日期-->
+  <div class="block" style="float:left"> 
+    <span class="demonstration">创建日期</span>
+    <el-date-picker
+      v-model="value2"
+      type="daterange"
+      align="right"
+      unlink-panels
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+      :picker-options="pickerOptions">
+    </el-date-picker>
+  &nbsp;&nbsp;&nbsp;</div>
+  <!--关键字搜索-->
+  <div style="width:200px;float:left">
+    <el-input v-model="input" placeholder="请输入关键字"></el-input>
+  </div>
+  <div  style="float:left">&nbsp;&nbsp;&nbsp;
+    <el-button @click="sel()" type="primary">搜索</el-button>
+  </div>
+  </div>
+    <el-table
     :data="tableData"
     style="width: 100%"
     max-height="250">
@@ -49,32 +84,99 @@
       label="操作"
       width="120">
     <template slot-scope="scope">
-        <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData)"
-          type="text"
-          size="small">
-          移除
-        </el-button>
+        <el-button @click="handleClick(scope.row)" type="text">库存分布</el-button>
+        <el-button type="text" >库存流水</el-button>
       </template>
     </el-table-column>
       
    
-  </el-table>
+    </el-table>
+  </div>
 </template>
 
 <script>
   export default {
-    methods: {
-      deleteRow(index, rows) {
-        rows.splice(index, 1);
+    data() {
+      return {  
+        tableData:[],
+        options:[],
+        //日期
+        pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+        value1: '',
+        value2: '',
+        input: '',
+        value3:'0',
       }
     },
-    data() {
-      return {
-        tableData: [{
-        
-        }]
+    methods: {
+      handleChange(e,form,thsAreaCode){
+          thsAreaCode=this.value3
+      },
+      sel:function(event){
+        this.axios.get('http://localhost:50774/api/InventorySels',{
+          params:{
+            WId:this.value3,
+            q:this.value1,
+            h:this.value2,
+            bian:this.input
+          }
+        })
+        .then(response=>{
+          this.tableData=response.data;
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+      },
+      handleClick(row) {
+        console.log(row);
       }
-    }
+    }, 
+    mounted(){
+        //显示       
+        this.axios.get('http://localhost:50774/api/InventorySels')
+          .then(response=>{
+            this.tableData=response.data;
+            console.log('ok')
+          })      
+          .catch(function(error){
+            console.log(error);
+          }) ,
+        //仓库下拉
+        this.axios.get('http://localhost:50774/api/SelectWare')
+        .then(response=>{
+          this.options=response.data;
+            console.log('ok')
+        })
+        .catch(function(error){
+            console.log(error);
+          })
+      }
   }
 </script>
