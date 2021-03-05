0a64ba42a29e65376ae592eaa49cafddca257539
1173090905@qq.com <1173090905@qq.com>
2021-03-05 18:42:21 +0800

    guo

--- src/components/ModelGuo/receiptPayments.vue
+++ src/components/ModelGuo/receiptPayments.vue
@@ -1,257 +1,252 @@
-// 付款单
+// 收款单
 <template>
   <div>
-    <div> <div>
+    <div>
+      <div>
         <table>
-            <tr>
-              <td><el-button size="mini" type="primary">新增</el-button></td>                  
-              <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<el-button size="mini" plain>批量操作</el-button></td>
-              <!-- <td><el-button size="mini" plain>导入</el-button></td>
+          <tr>
+            <td> <router-link to="/Addfkd"
+              ><el-button type="primary" size="mini"
+                >添加</el-button
+              ></router-link
+            ></td>
+            <td>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<el-button
+                size="mini"
+                plain
+                >批量操作</el-button
+              >
+            </td>
+            <!-- <td><el-button size="mini" plain>导入</el-button></td>
               <td><el-button size="mini" plain>导出</el-button></td> -->
-            </tr>
-            <br>
-            <tr>
-              <td>供应商:</td>
-              <td> 
-                <el-autocomplete
-               v-model.trim="state"
-              :fetch-suggestions="querySearchAsync"
-              
-               placeholder="客户名称/编号"
-               @select="handleSelect"
-               ></el-autocomplete>
-              </td>
-               
-              <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;付款日期:</td>
-              <td>
-              <div class="block">
-  
-    <el-date-picker
-      v-model="value"
-      type="daterange"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期"   
-      :default-time="['00:00:00', '23:59:59']">
-    </el-date-picker>
-  </div>
+          </tr>
+          <br />
+          <tr>
+            <td>供应商:</td>
+            <td>
+              <el-autocomplete
+                v-model="state"
+                :fetch-suggestions="querySearchAsync"
+                placeholder="客户名称/编号"
+                @select="handleSelect"
+              ></el-autocomplete>
+            </td>
 
-              </td>&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <td><el-input
-    placeholder="请输入单据编号"
-    prefix-icon="el-icon-search"
-    v-model="input2">    
-  </el-input></td>
-            </tr>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;付款日期:</td>
+            <td>
+              <div class="block">
+                <el-date-picker
+                  v-model="value"
+                  type="daterange"
+                  start-placeholder="开始日期"
+                  end-placeholder="结束日期"
+                  :default-time="['00:00:00', '23:59:59']"
+                >
+                </el-date-picker>
+              </div>
+            </td>
+            &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <td>
+              <el-input
+                placeholder="请输入单据编号"
+                prefix-icon="el-icon-search"
+                v-model="input2"
+              >
+              </el-input>
+            </td>
+          </tr>
         </table>
-     </div>
-     <el-table
-    :data="items"
-    style="width: 100%"
-    :row-class-name="tableRowClassName">
-    <el-table-column
-      type="selection"
-      width="55">
-    </el-table-column>
-    
-    <el-table-column
-      prop="rcreateTime"
-      label="日期"
-      width="180">
-    </el-table-column>
-     <el-table-column
-      prop="danJ"
-      label="单据编号"
-      width="180">
-    </el-table-column>
-     <el-table-column
-      prop="rppayer"
-      label="付款方"
-      width="180">
-    </el-table-column>
-     <el-table-column
-      prop="rcreateTime"
-      label="结算方式"
-      width="180">
-    </el-table-column>
-     <el-table-column
-      prop="rptotalPayment"
-      label="收款金额"
-      width="180">
-    </el-table-column>s
-     <el-table-column
-      prop="rptotalDiscount"
-      label="优惠金额"
-      width="180">
-    </el-table-column>
-     <el-table-column
-      prop="rptotalMoney"
-      label="实收金额"
-      width="180">
-    </el-table-column>
-       <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">详情</el-button>      
-      </template>
-    </el-table-column>
-  </el-table>
-     </div>
-      <div class="block">      
-    <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[1, 2, 3, 4]"
-      :page-size="1"
-      layout="total, sizes, prev, pager, next, jumper"       
-      :total="10">
-    </el-pagination>
+      </div>
+      <el-table
+        :data="items"
+        style="width: 100%"
+        :row-class-name="tableRowClassName"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+
+        <el-table-column prop="rcreateTime" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="danJ" label="单据编号" width="180">
+        </el-table-column>
+        <el-table-column prop="rppayer" label="付款方" width="180">
+        </el-table-column>
+        <el-table-column prop="rpmmode" label="结算方式" width="180">
+        </el-table-column>
+        <el-table-column prop="rpmpayment" label="收款金额" width="180">
+        </el-table-column
+        >s
+        <el-table-column prop="rpmdiscount" label="优惠金额" width="180">
+        </el-table-column>
+        <el-table-column prop="rpmmoney" label="实收金额" width="180">
+        </el-table-column>
+        <el-table-column fixed="right" label="操作" width="100">
+             <template slot-scope="scope">          
+        
+             <XQYM v-bind:id=scope.row.rpid></XQYM>
+             </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[1, 2, 3, 4]"
+        :page-size="2"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="10"
+      >
+      </el-pagination>
+    </div>
   </div>
-  </div>
-  
 </template>
 <script>
+ import XQYM from '@/components/ModelGuo/XQYM'
 // 节流函数
-const delay = (function() {
+const delay = (function () {
   let timer = 0;
-  return function(callback, ms) {
+  return function (callback, ms) {
     clearTimeout(timer);
     timer = setTimeout(callback, ms);
   };
 })();
- 
-export default {
-    data() {
-      return {        
-        isCollapse: true, 
-        items:[],
-        restaurants: [],
-        state: '',
-        timeout:  null,
-        input2:'',
-        //分页
-        currentPage1: 1,       //分页
-        currentPage2: 2,
-      
 
-         
-        value:'',
-         
-      };
-     }, 
-     watch: {
-  //watch title change
+export default {
+  data() {
+    return {
+      isCollapse: true,
+      items: [],
+      restaurants: [],
+      state: "",
+      timeout: null,
+      input2: "",
+      //分页
+      currentPage1: 1, //分页
+      currentPage2: 2,
+      rpidt:'',
+      value: "",
+    };
+  },
+  watch: {
+    //watch title change
     state() {
       delay(() => {
         this.fetchData();
       }, 300);
     },
-    input2(){
-      delay(()=>{
+    input2() {
+      delay(() => {
         this.fetchData();
-      },300);
+      }, 300);
     },
-    value(){
-      delay(()=>{        
+    value() {
+      delay(() => {
         this.fetchData();
-      
-      },300)
+      }, 300);
     },
-    currentPage1(){
+    currentPage1() {
       this.fetchData();
     },
-    currentPage2(){
+    currentPage2() {
       this.fetchData();
-    }
     },
-  
-     methods: {   
-        loadAll() {
-        return [
-           this.axios.get('http://localhost:50774/api/ClientSupplier')
-           .then(response => {
-            
-            this.restaurants=response.data;
-            })
-            .catch(function (error) {
-            console.log(error)
-                 })
-        ]},
-         handleSizeChange(val) {
-            this.currentPage1=val
-        },
-         handleCurrentChange(val) {
-            this.currentPage2=val
-        },
-         querySearchAsync(queryString, cb) {
-        var restaurants = this.restaurants;
-        var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
-         clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-          cb(results);
-        }, 3000 * Math.random());
-      },
-       createStateFilter(queryString) {
-        return (state) => {
-          return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-        };
-      },
-        handleSelect(item) {
-        console.log(item);
-      },
-      tableRowClassName({row, rowIndex}) {
-        if (rowIndex === 1) {
-          return 'warning-row';
-        } else if (rowIndex === 3) {
-          return 'success-row';
-        }
-        return '';
-      },
-       async fetchData(val) {
-      
-        const res = await  this.axios.get('http://localhost:50774/api/receiptPayments',{
-        params: {
-        FKF: this.state,
-        value: this.value[0],
-        value1: this.value[1],
-        DJ:this.input2,
-        datepage:this.currentPage1,
-        datesize:this.currentPage2
-      },
-      })
-        .then(response => {
-        this.items = response.data
-        
-      })       
+  },
+
+  methods: {
+    loadAll() {
+      return [
+        this.axios
+          .get("http://localhost:50774/api/ClientSupplier")
+          .then((response) => {
+            this.restaurants = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          }),
+      ];
+    },
+    handleSizeChange(val) {
+      this.currentPage1 = val;
+    },
+    handleCurrentChange(val) {
+      this.currentPage2 = val;
+    },
+    querySearchAsync(queryString, cb) {
+      var restaurants = this.restaurants;
+      var results = queryString
+        ? restaurants.filter(this.createStateFilter(queryString))
+        : restaurants;
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        cb(results);
+      }, 3000 * Math.random());
+    },
+    createStateFilter(queryString) {
+      return (state) => {
+        return (
+          state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+        );
+      };
+    },
+    handleSelect(item) {
+      console.log(item);
+    },
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
       }
- 
+      return "";
     },
-     
-    mounted () {
-         this.loadAll();
-        this.axios.get('http://localhost:50774/api/receiptPayments')
-        .then(response => {
-        this.items = response.data
-        console.log('ok')
+    async fetchData(val) {
+      const res = await this.axios
+        .get("http://localhost:50774/api/receiptPayments", {
+          params: {
+            FKF: this.state,
+            value: this.value[0],
+            value1: this.value[1],
+            DJ: this.input2,
+            datepage: this.currentPage1,
+            datesize: this.currentPage2,
+          },
+        })
+        .then((response) => {
+          this.items = response.data;
+        });
+    },
+     handleEdit(index, row) {
+          this.rpidt=row.rpid
+      },
+  },
+
+  mounted() {
+    this.loadAll();
+    this.axios
+      .get("http://localhost:50774/api/receiptPayments")
+      .then((response) => {
+        this.items = response.data;
+        console.log("ok");
       })
       .catch(function (error) {
-        console.log(error)
-    })
-   
+        console.log(error);
+      });
+      
+  },
+  components:{
+      "XQYM":XQYM
   }
-}
+};
 </script>
 
 <style>
-  .el-table .warning-row {
-    background: oldlace;
-  }
+.el-table .warning-row {
+  background: oldlace;
+}
 
-  .el-table .success-row {
-    background: #f0f9eb;
-  }  
-  
+.el-table .success-row {
+  background: #f0f9eb;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import shou from '@/components/shou'
-import XX from '@/components/views/XX'
-import XX2 from '@/components/views/XX2'
-import XX3 from '@/components/views/XX3'
 import sell from '@/components/views/sell'
 import sellreturn from '@/components/views/sellreturn'
 import receiptPayments from '@/components/ModelGuo/receiptPayments'
+import Addfkd from '@/components/ModelGuo/Addfkd'
+import Addfkdy from '@/components/ModelGuo/Addfkdy'
 import payment from '@/components/ModelGuo/payment'
 import SalesSingleShow from '@/components/ModelHan/SalesSingleShow'
 import SellCollect from '@/components/ModelHan/SellCollect'
@@ -21,36 +20,46 @@ export default new Router({
       path: '/',
       name: '首页',
       component: shou
-    } ,
+    },
     {
       path: '/Stock',
       name: '库存',
       component: inven
-    } ,
+    },
     {
       path: '/sellreturn',
       name: '销售退货申请单',
-      component:sellreturn,
-    },    
+      component: sellreturn,
+    },
     {
       path: '/zreceipt',
       name: '收款单',
-      component:receiptPayments,
-    } ,    
+      component: receiptPayments,
+    },
     {
       path: '/zpayment',
       name: '付款单',
-      component:payment,
+      component: payment,
     },
     {
       path: '/bdetail',
       name: '销售明细表',
-      component:SalesSingleShow,
+      component: SalesSingleShow,
     },
     {
       path: '/bcollect',
       name: '销售汇总表',
-      component:SellCollect,
+      component: SellCollect,
+    },
+    {
+      path: '/Addfkd',
+      name: '收款单添加',
+      component: Addfkd,
+    },
+    {
+      path: '/Addfkdy',
+      name: '付款单添加',
+      component: Addfkdy,
     }
 
   ]
